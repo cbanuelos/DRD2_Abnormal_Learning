1e51e376c4b34f321f2f3406688036d5139efe2d
Cristina <cbanuelo@andrew.cmu.edu>
2019-09-25 12:24:17 -0400

    Updated csv and code

--- DataAnalysisCode.py
+++ DataAnalysisCode.py
@@ -1,7 +1,16 @@
+import csv
 import numpy as np
+import pandas as pd
 
-data = np.loadtxt('DRD2_subset_data.dat', 
-					dtype = {'names': ('AGE','female_1', 'P', 'Q', 'VS','Obesity', 'DRD2','LABID'),
-							 'formats':('S1', 'S1', 'S1', 'S1','S1', 'S1', 'S1', 'S1')},
-					delimiter="	  ")
-data
+dataFrame = pd.read_csv('DRD2_subset_data.csv', delimiter=',')#dtype={'AGE':float,'female_1':float, 'P':float, 'Q':float, 'VS':float, 'Obesity':float, 'LABID':float})
+#data = dataFrame.dropna(subset=['P','Q','VS','DRD2'])
+#data = dataFrame.dropna()
+
+
+#dataFrame[~dataFrame.isin(['NaN', 'NaT']).any(axis=1)]
+
+dataFrame.replace('        NaN', 'NaN')
+dataFrame = dataFrame.dropna()
+
+#dataFrame.isnull().describe(pd.set_option('display.max_rows',466))
+print(dataFrame)
