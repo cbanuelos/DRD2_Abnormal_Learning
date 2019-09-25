81279493895e71cc7d3802365b31b129873a0c58
Cristina <cbanuelo@andrew.cmu.edu>
2019-09-25 10:17:44 -0400

    changed data file type

--- DataAnalysisCode.py
+++ DataAnalysisCode.py
@@ -1,1 +1,7 @@
-import numpy
+import numpy as np
+
+data = np.loadtxt('DRD2_subset_data.dat', 
+					dtype = {'names': ('AGE','female_1', 'P', 'Q', 'VS','Obesity', 'DRD2','LABID'),
+							 'formats':('S1', 'S1', 'S1', 'S1','S1', 'S1', 'S1', 'S1')},
+					delimiter="	  ")
+data
